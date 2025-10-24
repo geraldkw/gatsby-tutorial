de13e332f412fad086e4d8606fc7ff50b2ba15ad
W600119 <jwisniewski@woodmenlife.org>
2025-10-24 10:49:20 -0500

    trying to fix plugin crap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,10 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-const adapter = require('gatsby-adapter-netlify')
-
 module.exports = {
   siteMetadata: {
     title: `gatsby-tutorial`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-  adapter: adapter({
-    excludeDatastoreFromEngineFunction: false,
-  }),
 }
